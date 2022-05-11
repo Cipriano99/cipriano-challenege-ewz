5132c6c96f329416a7cd526a43d578a24a77b613
cipriano99 <samuelcipriano99@gmail.com>
2022-05-11 14:56:06 -0300

    Add Navbar for all aplication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
+import { Navbar } from '../components/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme} resetCSS>
+      <Navbar />
       <Component {...pageProps} />
     </ChakraProvider>
   )
