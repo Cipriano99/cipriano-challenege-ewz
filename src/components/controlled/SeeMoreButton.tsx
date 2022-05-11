0d5bd85cc17b027e91f0bb4cde1e8498148eb40e
cipriano99 <samuelcipriano99@gmail.com>
2022-05-11 16:51:41 -0300

    Created SeeMoreButton component

---
+++ src/components/controlled/SeeMoreButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@chakra-ui/react"
+
+export const SeeMoreButton = () => {
+  return (
+    <Button bg='ewz.400' color='ewz.100' _hover={{ bg: 'ewz.500' }}>
+      Ver mais
+    </Button>
+  )
+}
