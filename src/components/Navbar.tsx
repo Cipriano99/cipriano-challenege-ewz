718683ae3013791fc8c44ac2053072e257884977
cipriano99 <samuelcipriano99@gmail.com>
2022-05-11 17:47:08 -0300

    Improve Navbar responsivity

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { HStack, Link, Text } from "@chakra-ui/react"
 
 export const Navbar = () => {
   return (
-    <HStack h={20} w='full' bg='ewz.700' justify='space-between' px={76}>
+    <HStack h={20} w='full' bg='ewz.700' justify='space-between' px={{ base: 4, md: 12, lg: 20, xl: 76 }}>
       <Text fontSize={18} fontWeight='bold'>TodoTest</Text>
       <HStack>
         <Link fontSize={14} href="/">todos</Link>
